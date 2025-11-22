77db1ff91c14e4620cb4f9d2b9de7713bc9fe993
Daik <fruit@kd6.so-net.ne.jp>
2025-11-22 19:36:30 +0900

    Implement ship, hit and isSunk function

--- battleship.js
+++ battleship.js
@@ -1,5 +1,51 @@
-export function sum(a, b) {
-  return a + b;
+function ship(length) {
+  const hit = (id) => {
+    for (let i = 0; i < shipList.length; i++) {
+      const shipId = shipList[i].id;
+      
+      if (id === shipId) {
+        shipList[i].damage += 1;
+        
+        if (shipList[i].length === shipList[i].damage) {
+          isSunk(i);
+        }
+
+        break;
+      }
+    }
+  };
+
+  const isSunk = (i) => {
+    shipList[i].sunk = true;
+  };
+
+  return {
+    id: crypto.randomUUID(),
+    length,
+    damage: 0,
+    sunk: false,
+    hit,
+  };
 }
 
-module.exports = sum;
+function gameBoard() {
+  
+
+  return {
+
+  }
+}
+
+const shipOne = ship(4);
+const shipTwo = ship(3);
+const shipThree = ship(2);
+const shipFour = ship(1);
+const shipList = [shipOne, shipTwo, shipThree, shipFour];
+
+shipOne.hit(shipTwo.id);
+shipOne.hit(shipTwo.id);
+shipOne.hit(shipThree.id);
+shipOne.hit(shipFour.id);
+console.log(shipList);
+
+module.exports = ship;
