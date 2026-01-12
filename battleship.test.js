aaced70e33da52ab0f6d02b9c87c64497a579152
Daik <fruit@kd6.so-net.ne.jp>
2026-01-12 20:21:28 +0900

    Complete the project as the first version

--- battleship.test.js
+++ battleship.test.js
@@ -1,9 +1,8 @@
-/* global require: writable */
 const { describe, test, expect } = require('@jest/globals');
-const sum = require('./src/battleship.js');
+const gameBoard = require('./src/battleship.js');
 
-describe('gameBoard function', () => {
-  test('example 1', () => {
-    expect(sum(4, 5)).toBe(9);
+describe('gameBoard', () => {
+  test('deployBoard', () => {
+    expect(gameBoard().deployBoard([{ length: 4, direction: 'horizontal', coordinates: [[0, 1], [0, 2], [0, 3], [0, 4]] }, { length: 3, direction: 'vertical', coordinates: [[5, 3], [6, 3], [7, 3]] }, { length: 2, direction: 'horizontal', coordinates: [[3, 6], [3, 7]] }, { length: 1, direction: 'vertical', coordinates: [[6, 1]] }])).toEqual([[0, 'S', 'S', 'S', 'S', 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 'S', 'S'], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 'S', 0, 0, 0, 0], [0, 'S', 0, 'S', 0, 0, 0, 0], [0, 0, 0, 'S', 0, 0, 0, 0]]);
   });
 });
